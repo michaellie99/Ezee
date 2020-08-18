52d76a9a7bfbd371c5c9f5e15407159eb9118702
Michael <michaellie1999@gmail.com>
2020-08-18 13:25:16 +0700

    Initial Commit

---
+++ Ezee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ezee
+//
+//  Created by Michael Lie on 18/08/20.
+//  Copyright © 2020 Michael Lie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
